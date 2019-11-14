ecb67382015f5b9995a80f2770b89de5fed45865
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-11-14 11:35:02 -0600

    updated server.js and server package.json, removed some scripts from package.json

---
+++ client/build/precache-manifest.0554a99df24b59a9fe691a27e49695be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b57b0a9f455ed7186a76fbd414b4e5b",
+    "url": "/static/media/sunSet.9b57b0a9.gif"
+  },
+  {
+    "revision": "36eec61c2efbc0be0d6168bde3e598fb",
+    "url": "/static/media/darkbluebackground1.36eec61c.png"
+  },
+  {
+    "revision": "db9bd38c1a3628518ead0f130a42aac4",
+    "url": "/static/media/8bitPC.db9bd38c.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6bcfd80cbe6f16e418ae",
+    "url": "/static/js/main.4f7cc958.chunk.js"
+  },
+  {
+    "revision": "c177c7c025e3f8d31c8b",
+    "url": "/static/js/2.94154ff3.chunk.js"
+  },
+  {
+    "revision": "6bcfd80cbe6f16e418ae",
+    "url": "/static/css/main.e7b55034.chunk.css"
+  },
+  {
+    "revision": "c177c7c025e3f8d31c8b",
+    "url": "/static/css/2.90c1705e.chunk.css"
+  },
+  {
+    "revision": "5befc8d93615d5a19f3bcef1fc11d6b0",
+    "url": "/index.html"
+  }
+];
