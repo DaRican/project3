ab6ad6985db73e13aee2c5697264a82b7724914a
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-11-14 12:50:55 -0600

    commented out or removed import axios

--- client/src/components/education-list.component.js
+++ client/src/components/education-list.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import '../css/list-education.css';
 import API from '../utils/API';
 
