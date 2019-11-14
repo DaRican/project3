97b0f6c43ab0f621ebc0f7c84d19ef69ca91d320
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-11-14 12:02:00 -0600

    removed heroku-postbuild

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client"));
+    app.use(express.static("client/public"));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "public", "index.html"))
